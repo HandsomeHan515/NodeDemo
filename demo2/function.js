af62d5de2485a9b6b1d62d28cbcc2ab34e18d740
hanshuai <hanshuai@dongwu-inc.com>
2017-06-22 08:18:27 +0800

    add function

---
+++ demo2/function.js
@@ -0,0 +1,5 @@
+const fun2 = res => {
+  res.write('Hello, I am function2.\n');
+}
+
+module.exports = fun2;
---
+++ demo2/index.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const fun2 = require('./function.js');
+const hostname = '127.0.0.1';
+const port = '3000';
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  fun1(res);
+  fun2(res);
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`服务器运行在 http://${hostname}:${port}`);
+});
+
+const fun1 = res => {
+  res.write('Hello, I am function.\n');
+}
